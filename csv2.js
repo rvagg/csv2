8235d7be237402673a236dc4f78944719daaeaf5
Matteo Collina <matteo.collina@gmail.com>
2014-05-10 11:46:11 +0200

    Depend directly on the bundled readable-stream.

--- csv2.js
+++ csv2.js
@@ -1,4 +1,4 @@
-const Transform = require('stream').Transform || require('readable-stream/transform')
+const Transform = require('readable-stream/transform')
     , inherits  = require('util').inherits
 
 function CSV2 (options) {
